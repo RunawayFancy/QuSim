c51a185ec20467ed39817de2cd5691e74ff46997
Runaway_Fancy <54537763+RunawayFancy@users.noreply.github.com>
2024-06-23 22:39:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qusim',
-    version='1.0.0',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
         'matplotlib==3.8.3',
@@ -12,7 +12,10 @@ setup(
         'qutip==4.7.5',
         'scipy==1.12.0',
         'sympy==1.12',
-        'tqdm==4.66.2'
+        'tqdm==4.66.2',
+        'ipython==8.22.2',
+        'ipykernel==6.29.3',
+        'ipywidgets==7.6.5'
     ],
     author='Jiheng Duan',
     author_email='jiheng.duan@rochester.edu',
