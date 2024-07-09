20090160a563962fe14156ac6dc9b7b738516367
Runaway_Fancy <54537763+RunawayFancy@users.noreply.github.com>
2024-07-09 00:32:11 -0400

    1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qusim',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     install_requires=[
         'matplotlib==3.8.3',
